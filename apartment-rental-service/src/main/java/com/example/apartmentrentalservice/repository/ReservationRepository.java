5920fbf30c2d7f48640c50ca80cfe527b7659adf
aleksanderinglot <aleksanderinglot@hotmail.com>
2023-06-27 09:17:05 +0200

    Implement services

--- apartment-rental-service/src/main/java/com/example/apartmentrentalservice/repository/ReservationRepository.java
+++ apartment-rental-service/src/main/java/com/example/apartmentrentalservice/repository/ReservationRepository.java
@@ -4,6 +4,11 @@ import com.example.apartmentrentalservice.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByApartmentIdAndIdNot(Long apartmentId, Long id);
+    List<Reservation> findByApartmentId(Long apartmentId);
+    List<Reservation> findByGuestEmail(String guestEmail);
 }
