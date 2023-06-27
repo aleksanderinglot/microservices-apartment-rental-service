064d89d14bd1bc23448568801e0492153150f04b
aleksanderinglot <57831465+aleksanderinglot@users.noreply.github.com>
2023-06-27 18:33:11 +0200

    Create config server module

---
+++ apartment-rental-service-config-server/src/main/java/com/example/apartmentrentalserviceconfigserver/ApartmentRentalServiceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentRentalServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentRentalServiceConfigServerApplication.class, args);
+	}
+
+}
---
+++ apartment-rental-service-config-server/src/test/java/com/example/apartmentrentalserviceconfigserver/ApartmentRentalServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalserviceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentRentalServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
