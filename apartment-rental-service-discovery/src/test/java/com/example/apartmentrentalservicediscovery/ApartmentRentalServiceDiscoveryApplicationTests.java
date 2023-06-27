37bd6ec25137c50f7055b10f89551f03c58f868d
aleksanderinglot <57831465+aleksanderinglot@users.noreply.github.com>
2023-06-27 18:45:23 +0200

    Create discovery module

---
+++ apartment-rental-service-discovery/src/test/java/com/example/apartmentrentalservicediscovery/ApartmentRentalServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentRentalServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
