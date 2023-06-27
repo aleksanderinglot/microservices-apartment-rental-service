f29b699787824e4717f9ff43cf225f033a332f66
aleksanderinglot <57831465+aleksanderinglot@users.noreply.github.com>
2023-06-27 18:50:09 +0200

    Create gateway module

---
+++ apartment-rental-service-gateway/src/main/java/com/example/apartmentrentalservicegateway/ApartmentRentalServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentRentalServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentRentalServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ apartment-rental-service-gateway/src/test/java/com/example/apartmentrentalservicegateway/ApartmentRentalServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalservicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentRentalServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
